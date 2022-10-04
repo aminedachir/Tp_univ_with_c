bdabba4a0fda469cd9d87533b239d3a7b3411f0f
aminedachir <dachiramine110>
2022-10-04 19:12:39 +0200

    donnez les valeurs de a et b et c

--- Tp_0/home_work.c
+++ Tp_0/home_work.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+int a,b,c;
 int main ()
 {
-
+    printf("Entrez la valeur de a:");
+    scanf("%d", &a);
+    printf("Entrez la valeur de b:");
+    scanf("%d", &b);
+    printf("Entrez la valeur de c:");
+    scanf("%d", &c);
 }
