7ab33ffce4edeab8dda84b3443a7bb37f112e895
aminedachir <dachiramine110>
2022-10-04 19:47:29 +0200

    problem

--- Tp_0/home_work.c
+++ Tp_0/home_work.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int a,b,c;
+int a,b,c,delta,X1,X2,jdr1,jdr2;
 int main ()
 {
     printf("Entrez la valeur de a:");
@@ -8,4 +8,10 @@ int main ()
     scanf("%d", &b);
     printf("Entrez la valeur de c:");
     scanf("%d", &c);
+    delta = b*b-4*a*c;
+    printf("%d\n", delta);
+    jdr1 = -b-(delta/8);
+    jdr2 = 2*a;
+    X1 = jdr1/jdr2;
+    printf("%d", X1);
 }
