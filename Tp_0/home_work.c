1e61f9c6679f0cae98e5498088b48d65baf082fb
aminedachir <dachiramine110>
2022-10-05 17:27:01 +0200

    Delta home work

--- Tp_0/home_work.c
+++ Tp_0/home_work.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int a,b,c,delta,X1,X2;
+int a,b,c,delta,X1,X2,X;
 int main ()
 {
     printf("Entrez la valeur de a:");
@@ -10,8 +10,19 @@ int main ()
     scanf("%d", &c);
     delta = b*b-4*a*c;
     printf("Delta : %d\n", delta);
-    X1 = (-b-(sqrt(delta)))/(2*a);
-    printf("X1 : %d\n", X1);
-    X2 = (-b+(sqrt(delta)))/(2*a);
-    printf("X2 : %d", X2);
+    if (delta > 0){
+        printf("Il y a deux solutions\n");
+        X1 = (-b-(sqrt(delta)))/(2*a);
+        printf("X1 : %d\n", X1);
+        X2 = (-b+(sqrt(delta)))/(2*a);
+        printf("X2 : %d", X2);
+    }
+    else if (delta == 0){
+        X = ((-b)/2*a);
+        printf("Il y a un double solutions \n");
+        printf("X : %d\n", X);
+    }
+    else {
+        printf("Il y a Pas de solution\n");
+    }
 }
