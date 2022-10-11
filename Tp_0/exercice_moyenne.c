bea2e4f795db9a1fd60e86e08afbf844ea05b10b
aminedachir <dachiramine110>
2022-10-11 11:29:57 +0200

    exercice 5

---
+++ Tp_0/exercice_moyenne.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int a,b,c,d,moyenne;
+int main ()
+{
+    printf("donnez la valeur de a : ");
+    scanf("%d", &a);
+    printf("donnez la valeur de b : ");
+    scanf("%d", &b);
+    printf("donnez la valeur de c : ");
+    scanf("%d", &c);
+    printf("donnez la valeur de d : ");
+    scanf("%d", &d);
+    moyenne = (a+b+c+d)/4;
+    printf("Moyenne : %d\n",moyenne);
+    if (moyenne > 10 && moyenne < 20){
+        printf("succed");
+    }
+    else if  (moyenne > 0 && moyenne < 10) {
+        printf("Failed");
+    }
+    else {
+        printf("Error");
+    }
+}
