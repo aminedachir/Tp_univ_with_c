618cbe87a6cdcddd2faded93b6d00c834577efa8
aminedachir <dachiramine110>
2022-10-18 11:20:51 +0200

    valeur-absolue

--- Tp_2/Valeur_absolue.c
+++ Tp_2/Valeur_absolue.c
@@ -5,8 +5,8 @@ int main ()
     printf("Donnez un num : ");
     scanf("%d", &number);
     if (number < 0) {
-        number = (-1)*(number);
-        printf("La valeur absolue est : %d", number);
+
+        printf("La valeur absolue est : %d",- number);
     }
     else {
         printf("La valeur absolue est : %d", number);
