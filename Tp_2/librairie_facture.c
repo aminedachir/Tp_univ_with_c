7aa1f25e9da57fac13ac319580d941b898db898f
aminedachir <dachiramine110>
2022-10-23 20:00:18 +0200

    exo3

--- Tp_2/librairie_facture.c
+++ Tp_2/librairie_facture.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-int num_des_copies,prix,prix1,prix2,copies;
-
-int main ()
-{
+int num_des_copies,prix,prix_5DA,prix_4DA,prix_3DA;
+int main () {
     printf("Entrez le num des copies : ");
     scanf("%d", &num_des_copies);
-    if (num_des_copies > 30){
-        copies = num_des_copies-20;
-        prix2 = copies*3;
+    if (num_des_copies > 0){
+        for (int i=0;i<=10;i++){
+            prix_3DA = i*5;
+        }
+        for (int i=10;i<=30;i++){
+            prix_4DA = i*4;
+        }
+        for (int i=0;i<num_des_copies-30;i++){
+            prix_5DA = i*5;
+        }
     }
-    else if ((copies > 10) && (copies <= 30)) {
-        copies = num_des_copies-10;
-        prix1 = copies*4;
-    }
-    else if (copies <= 10) {
-            prix = num_des_copies*5;
-    }
-    else {
-        printf("Error");
-    }
-    prix = prix + prix1 + prix2;
+    prix = prix_5DA + prix_4DA + prix_3DA;
+    printf("%d\n", prix_5DA);
+    printf("%d\n", prix_4DA);
+    printf("%d\n", prix_3DA);
     printf("%d", prix);
 }
--- Tp_2/librairie_facture_sol2.c
+++ Tp_2/librairie_facture_sol2.c
@@ -8,10 +8,10 @@ int main () {
             prix_5DA = i*5;
             }
         else if ((i>10) && (i<=30)){
-            prix_4DA = i*4;
+            prix_4DA = (i-10)*4;
         }
         else if (i>30){
-            prix_3DA = (i-40)*3;
+            prix_3DA = (i-30)*3;
         }
     }
     prix = prix_3DA+prix_4DA+prix_5DA;
