7dc4587fcf8d0d6ee50caa3a066dc06c20056b34
aminedachir <dachiramine110>
2022-10-24 19:51:24 +0200

    Porblem solved

--- Tp_2/librairie_facture.c
+++ Tp_2/librairie_facture.c
@@ -4,19 +4,16 @@ int main () {
     printf("Entrez le num des copies : ");
     scanf("%d", &num_des_copies);
     if (num_des_copies > 0){
-        for (int i=0;i<=10;i++){
-            prix_3DA = i*5;
-        }
-        for (int i=10;i<=30;i++){
-            prix_4DA = i*4;
-        }
-        for (int i=0;i<num_des_copies-30;i++){
+        for (int i=0;i<=num_des_copies && (i<=10);i++){
             prix_5DA = i*5;
+            }
+        for (int i=0;i<=(num_des_copies-10) && (i<=20);i++){
+            prix_4DA = i*4;
+            }
+        for (int i=0;i<=(num_des_copies-30);i++){
+            prix_3DA = i*3;
+            }
         }
-    }
     prix = prix_5DA + prix_4DA + prix_3DA;
-    printf("%d\n", prix_5DA);
-    printf("%d\n", prix_4DA);
-    printf("%d\n", prix_3DA);
-    printf("%d", prix);
+    printf("La facture est : %d DA", prix);
 }
