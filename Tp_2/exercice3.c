c9452e107baffb426bf391f121c4bc8268c84b5b
aminedachir <dachiramine110>
2022-10-25 12:03:59 +0200

    Exercice 3

--- Tp_2/exercice3.c
+++ Tp_2/exercice3.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+int year;
 int main ()
 {
-
+    printf(" Entrez une année : ");
+    scanf("%d", &year);
+    if ((year%4 == 0) && ((year%400 == 0)||(year%100 != 0))) {
+        printf("une année bissextille");
+        }
+    else {
+        printf("une année normal");
+    }
 }
