390d0524ef1106d01c55a5d1b874e17d2a24e749
aminedachir <dachiramine110>
2022-12-13 19:18:42 +0100

    tp 4

--- Tp_4/search.c
+++ Tp_4/search.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 char ch[50];
-int s_vo;
+int s_vo,s_co,s_no,x;
 int main()
 {
     s_vo = 0;
+    s_co = 0;
+    s_no = 0;
+    printf("Entrez un Mot :");
     scanf("%s",&ch);
-    for(int i=0;i<50;i++){
-        if(ch[i]=="a" || ch[i]=="e" || ch[i]=="u" || ch[i]=="o" || ch[i]=="i" || ch[i]=="y"){
+    for(int i=0;i<strlen(ch);i++){
+        if((ch[i]=='a' || ch[i]=='e') || (ch[i]=='u' || ch[i]=='o') || (ch[i]=='i' || ch[i]=='y')){
             s_vo = s_vo + 1;
         }
+        else{
+            s_co = s_co + 1;
+        }
+        x = atoi(ch[i]);
+        if((x*2)%2==0){
+            s_no = s_no + 1;
+        }
     }
-    printf("%d",s_vo);
+    printf("nombre de voi : %d\n",s_vo);
+    printf("nombre de const : %d",s_co);
+    printf("nombre des nombres : %d",s_no);
 }
