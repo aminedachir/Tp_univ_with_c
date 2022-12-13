4f9d4760ea4c476829f0a48e7e0f1a938cccada6
aminedachir <dachiramine110>
2022-12-13 20:33:07 +0100

    solve problem using ASCI Table

--- Tp_4/search.c
+++ Tp_4/search.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 char ch[50];
-int s_vo,s_co,s_no,x;
+int s_vo,s_co,s_no,s_char;
 int main()
 {
     s_vo = 0;
@@ -13,15 +13,19 @@ int main()
         if((ch[i]=='a' || ch[i]=='e') || (ch[i]=='u' || ch[i]=='o') || (ch[i]=='i' || ch[i]=='y')){
             s_vo = s_vo + 1;
         }
+        else if(((int)ch[i]<=57) && ((int)ch[i]>=48)){
+            s_no = s_no + 1;
+        }
+        else if((((int)ch[i]<=47) && ((int)ch[i]>=32)) || (((int)ch[i]<=64) && ((int)ch[i]>=58)) || (((int)ch[i]<=96) && ((int)ch[i]>=91))){
+            s_char = s_char + 1;
+        }
         else{
             s_co = s_co + 1;
         }
-        x = atoi(ch[i]);
-        if((x*2)%2==0){
-            s_no = s_no + 1;
-        }
+
     }
     printf("nombre de voi : %d\n",s_vo);
-    printf("nombre de const : %d",s_co);
-    printf("nombre des nombres : %d",s_no);
+    printf("nombre de const : %d\n",s_co);
+    printf("nombre des nombres : %d\n",s_no);
+    printf("nombre des char : %d\n",s_char);
 }
