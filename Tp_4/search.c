530eb7408a0a4760d3a3ece065ce7fc3064d9052
aminedachir <dachiramine110>
2022-12-13 20:36:02 +0100

    solve problem using ASCI Table

--- Tp_4/search.c
+++ Tp_4/search.c
@@ -7,22 +7,22 @@ int main()
     s_vo = 0;
     s_co = 0;
     s_no = 0;
+    s_char = 0;
     printf("Entrez un Mot :");
     scanf("%s",&ch);
     for(int i=0;i<strlen(ch);i++){
         if((ch[i]=='a' || ch[i]=='e') || (ch[i]=='u' || ch[i]=='o') || (ch[i]=='i' || ch[i]=='y')){
-            s_vo = s_vo + 1;
+            s_vo+=1;
         }
         else if(((int)ch[i]<=57) && ((int)ch[i]>=48)){
-            s_no = s_no + 1;
+            s_no+=1;
         }
         else if((((int)ch[i]<=47) && ((int)ch[i]>=32)) || (((int)ch[i]<=64) && ((int)ch[i]>=58)) || (((int)ch[i]<=96) && ((int)ch[i]>=91))){
-            s_char = s_char + 1;
+            s_char+=1;
         }
         else{
-            s_co = s_co + 1;
+            s_co+=1;
         }
-
     }
     printf("nombre de voi : %d\n",s_vo);
     printf("nombre de const : %d\n",s_co);
