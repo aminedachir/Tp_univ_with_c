79fef70b7d9acfbe4ca2fdc0948708bb42810b52
dachiramine110@gmail.com <dachiramine110@gmail.com>
2023-01-31 20:16:27 +0100

    Finish

--- semastre 2/Tp_01/enregistrement.c
+++ semastre 2/Tp_01/enregistrement.c
@@ -10,32 +10,36 @@ struct person{
     int matricule;
     float moyenne;
     struct date d1;
-    //struct table[5];
+
 };
 int main(){
-    struct person p1;
 // Les donnes de person
+    struct person table[5]={
+
     for (int i=0;i<5;i++){
         printf("Entrez votre nome : ");
-        scanf("%s", &p1.nome);
+        scanf("%s", &table.nome);
         printf("Entrez votre prenome : ");
-        scanf("%s", &p1.prenome);
+        scanf("%s", &table.prenome);
         printf("Entrez votre matricule : ");
-        scanf("%d", &p1.matricule);
+        scanf("%d", &table.matricule);
         printf("Entrez votre moyenne : ");
-        scanf("%d", &p1.moyenne);
+        scanf("%d", &table.moyenne);
         printf("Entrez votre jour de naissance : ");
-        scanf("%d",&p1.d1.jour);
+        scanf("%d",&table.d1.jour);
         printf("Entrez votre mois de naissance : ");
-        scanf("%s",&p1.d1.mois);
+        scanf("%s",&table.d1.mois);
         printf("Entrez votre annee de naissance : ");
-        scanf("%d",&p1.d1.annee);
-    }
+        scanf("%d",&table.d1.annee);
+
+
+    };
+     };
  //L'affichage de Les information d'un person
-    printf("Le nome est : %s\n" ,p1.nome);
-    printf("Le prenome est : %s\n" ,p1.prenome);
-    printf("Le matricule est : %d\n" ,p1.matricule);
-    printf("La date de naissance : %d/%s/%d\n",p1.d1.jour,p1.d1.mois,p1.d1.annee);
+    printf("Le nome est : %s\n" ,table.nome);
+    printf("Le prenome est : %s\n" ,table.prenome);
+    printf("Le matricule est : %d\n" ,table.matricule);
+    printf("La date de naissance : %d/%s/%d\n",table.d1.jour,table.d1.mois,table.d1.annee);
     return 0;
 
 
