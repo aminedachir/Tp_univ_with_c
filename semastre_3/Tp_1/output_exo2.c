a021bf7cc8e528b4c6a605dbf8d5cf9ae65d3e4d
aminedachir <dachiramine110@gmail.com>
2024-12-08 20:27:37 +0100

    add input file and output file of exo 2

---
+++ semastre_3/Tp_1/output_exo2.c
@@ -0,0 +1,4 @@
+<----------This is The table reversed with another table--------->
+15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 
+<--------This is The table reversed without another table-------->
+15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 
